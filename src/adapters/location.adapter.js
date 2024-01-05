59d1648d7b74e01551e40676a1fab17292e4f162
Esteban Sayago <sayagoo823@gmail.com>
2024-01-05 00:48:04 -0300

    delete adapters

--- src/adapters/location.adapter.js
+++
@@ -1,9 +0,0 @@
-function location(jsonData) {
-  return {
-    city: jsonData.location.name,
-    region: jsonData.location.region,
-    country: jsonData.location.country,
-  };
-}
-
-export { location };
