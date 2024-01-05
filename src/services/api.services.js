c18160475a867b74ecf1f1dea3e385ff94c37535
Esteban Sayago <sayagoo823@gmail.com>
2024-01-05 00:55:19 -0300

    create api services

---
+++ src/services/api.services.js
@@ -0,0 +1,23 @@
+import { base_url, api_key } from "../config/config";
+
+class ApiServices {
+  async currentData(location) {
+    return await fetch(
+      `${base_url}/current.json?key=${api_key}&q=${location}&aqi=no`
+    ).then((res) => res.json());
+  }
+
+  async todayData(location) {
+    return await fetch(
+      `${base_url}/forecast.json?key=${api_key}&q=${location}&days=1`
+    ).then((res) => res.json());
+  }
+
+  async next3DaysData(location) {
+    return await fetch(
+      `${base_url}/forecast.json?key=${api_key}&q=${location}&days=3`
+    ).then((res) => res.json());
+  }
+}
+
+export default new ApiServices();
