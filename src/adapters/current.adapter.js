54effc6fb2c00bee2735072616a6096e7a5f5137
Esteban Sayago <sayagoo823@gmail.com>
2024-01-05 00:48:23 -0300

    create adapter

---
+++ src/adapters/current.adapter.js
@@ -0,0 +1,27 @@
+function current(data) {
+  return {
+    location: location(data.location),
+    current: {
+      time: data.location.localtime,
+      temp_c: data.current.temp_c,
+      is_day: data.current.is_day,
+      wind_kph: data.current.wind_kph,
+      pressure_mb: data.current.pressure_mb,
+      humidity: data.current.humidity,
+      feelslike_c: data.current.feelslike_c,
+      vis_km: data.current.vis_km,
+      uv_index: data.current.uv,
+      condition: data.current.condition.text,
+    },
+  };
+}
+
+export { current };
+
+function location(location) {
+  return {
+    city: location.name,
+    region: location.region,
+    country: location.country,
+  };
+}
---
+++ src/adapters/forecastday.adapter.js
@@ -0,0 +1,33 @@
+function forecastday(forecastday) {
+  return {
+    dayInfo: {
+      date: forecastday.date,
+      maxtemp_c: forecastday.day.maxtemp_c,
+      mintemp_c: forecastday.day.mintemp_c,
+      chance_of_rain: forecastday.day.daily_chance_of_rain,
+      condition: forecastday.day.condition.text,
+      uv_index: forecastday.day.uv,
+    },
+    hour: forecastday.hour.map((data) => hour(data)),
+    astro: astro(forecastday.astro),
+  };
+}
+export { forecastday };
+
+function astro(astroProperty) {
+  return {
+    sunrise: astroProperty.sunrise,
+    sunset: astroProperty.sunset,
+  };
+}
+
+function hour(hourObject) {
+  return {
+    time: hourObject.time.substring(11),
+    temp_c: hourObject.temp_c,
+    is_day: hourObject.is_day,
+    condition: hourObject.condition.text,
+    chance_of_rain: hourObject.chance_of_rain,
+    uv_index: hourObject.uv,
+  };
+}
