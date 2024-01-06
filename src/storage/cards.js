951c3d0e549ae6012ebdb9a39fb781934e5e91d2
Esteban Sayago <sayagoo823@gmail.com>
2024-01-06 13:10:29 -0300

    add information about main cards

---
+++ src/storage/cards.js
@@ -0,0 +1,36 @@
+const cards = [
+  {
+    title: "UV Index",
+    icon: "☀️",
+  },
+  {
+    title: "Wind",
+    icon: "🌀",
+  },
+  {
+    title: "Humidity",
+    icon: "🚿",
+  },
+  {
+    title: "Visibility",
+    icon: "👁️",
+  },
+  {
+    title: "Feels like",
+    icon: "💉",
+  },
+  {
+    title: "Chance of rain",
+    icon: "💧",
+  },
+  {
+    title: "Pressure",
+    icon: "🕑",
+  },
+  {
+    title: "Sunset",
+    icon: "🌇",
+  },
+];
+
+export { cards };
