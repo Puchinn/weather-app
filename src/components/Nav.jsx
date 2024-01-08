8299d26c62f2c904a38052535bacd981119cc71e
Esteban Sayago <sayagoo823@gmail.com>
2024-01-08 18:26:57 -0300

    use custom colors

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,8 @@
 function Nav() {
   return (
-    <nav className="bg-[#282828] max-w-max p-3 rounded-xl h-svh">
+    <nav className="bg-light-primary-dark max-w-max p-3 rounded-xl h-svh">
       <ul className="text-center text-sm space-y-5">
-        <li className="rounded-xl bg-[#383838] p-1 px-3 my-10">
+        <li className="rounded-xl bg-primary-dark-hover p-1 px-3 my-10">
           <span className="text-2xl">🏠</span>
           <p>Home</p>
         </li>
