533f27c347a89942e4322507f6b48d7187040e3f
Esteban Sayago <sayagoo823@gmail.com>
2024-01-10 16:18:50 -0300

    add logic to display dinamic time

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,9 +1,24 @@
-function Clock() {
+import { useState, useEffect } from "react";
+
+function Clock({ initialTime }) {
+  const settedHours = new Date().setHours(
+    initialTime.substring(11).split(":")[0]
+  );
+  const [time, setTime] = useState(new Date(settedHours));
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime((prevState) => new Date(prevState.getTime() + 1000));
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const formattedTime = time.toLocaleTimeString().replaceAll(":", " | ");
+
   return (
     <div className="py-14 max-w-sm mx-auto bg-light-primary-dark rounded-md">
-      <p className="text-5xl text-center">
-        {new Date().toLocaleTimeString()} hs
-      </p>
+      <p className="text-5xl text-center">{formattedTime}</p>
     </div>
   );
 }
