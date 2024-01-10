ec8a2d7e7b30ef330556ac1745df2e7f5dfaa433
Esteban Sayago <sayagoo823@gmail.com>
2024-01-10 16:22:14 -0300

    use customs hooks and pass data to components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,18 +3,34 @@ import { CardList as CurrentCardsList } from "../components/currentWeather/CardL
 import { CardList as TodayCardsList } from "../components/todaysWeather/CardList";
 import { Forecast } from "../components/forecastInfo/Forecast";
 import { Clock } from "../components/Clock";
+import { useFetchDataMain } from "../hooks/useFetchDataMain";
+import { useDataMain } from "../hooks/useDataMain";
 
 function Home() {
+  const { isLoadingData } = useFetchDataMain();
+  const {
+    current,
+    location,
+    formatedArrayCards,
+    arrayToTodayWeather,
+    forecastDays,
+  } = useDataMain();
+
+  /* TODO: add loading skeleton here... */
+  if (isLoadingData) {
+    return <h1>loading data...</h1>;
+  }
+
   return (
     <div className="grid grid-cols-12 gap-x-10 pb-10">
       <div className="col-span-7">
-        <MainInfo></MainInfo>
-        <CurrentCardsList></CurrentCardsList>
+        <MainInfo current={current} location={location}></MainInfo>
+        <CurrentCardsList formatedArray={formatedArrayCards}></CurrentCardsList>
       </div>
       <div className="col-span-5 space-y-5">
-        <TodayCardsList></TodayCardsList>
-        <Forecast></Forecast>
-        <Clock></Clock>
+        <TodayCardsList arrayOfData={arrayToTodayWeather}></TodayCardsList>
+        <Forecast data={forecastDays}></Forecast>
+        <Clock initialTime={current.time}></Clock>
       </div>
     </div>
   );
