03a7faafb5a8066893148e7fbf977696c0b4bdc1
Esteban Sayago <sayagoo823@gmail.com>
2024-01-12 13:59:16 -0300

    update layout

--- src/pages/layout/AppLayout.jsx
+++ src/pages/layout/AppLayout.jsx
@@ -1,13 +1,14 @@
-import { Nav } from "../../components/Nav";
+import { Nav } from "../../components/navbar/Nav";
 import { Search } from "../../components/Search";
+import { Outlet } from "react-router-dom";
 
-function AppLayout({ children }) {
+function AppLayout() {
   return (
     <div className="max-w-7xl mx-auto flex gap-x-5">
       <Nav></Nav>
       <div className="w-full">
         <Search></Search>
-        {children}
+        <Outlet />
       </div>
     </div>
   );
