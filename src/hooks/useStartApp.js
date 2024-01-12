40d9b47fe4fca6a71fdf80904680e82a4bc12bd3
Esteban Sayago <sayagoo823@gmail.com>
2024-01-12 12:01:23 -0300

    update hook, use local storage util function

--- src/hooks/useStartApp.js
+++ src/hooks/useStartApp.js
@@ -1,17 +1,33 @@
 import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { userSettingsActions } from "../redux/reducers/userSettings";
+import userSettingsLocalStorage from "../utils/userSettingsLocalStorage";
 
 function useStartApp() {
+  const dispatch = useDispatch();
   const [hasSavedConfig, setHasSavedConfig] = useState(false);
+  const [loadingData, setLoadingData] = useState(true);
+  const state = useSelector((state) => state.userSettings);
 
   useEffect(() => {
-    console.log("checking local storage...");
-    console.log("user has settings saved");
-    console.log("fetching data...");
-    console.log("setting redux config states...");
-    setHasSavedConfig(true);
-  }, []);
+    if (state.location.city.length > 0) {
+      setHasSavedConfig(true);
+      setLoadingData(false);
+      return;
+    }
+    const userSettings = userSettingsLocalStorage.settings();
+    if (!userSettings.location) {
+      setHasSavedConfig(false);
+      setLoadingData(false);
+    } else {
+      dispatch(userSettingsActions.setSettings(userSettings));
+      setHasSavedConfig(true);
+      setLoadingData(false);
+      console.log("user has settings");
+    }
+  }, [dispatch, state]);
 
-  return { hasSavedConfig };
+  return { hasSavedConfig, loadingData };
 }
 
 export { useStartApp };
