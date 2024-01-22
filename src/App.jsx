e1f85b212d6100b74cb59403e9ef5818efeb21c5
Esteban Sayago <sayagoo823@gmail.com>
2024-01-22 00:01:48 -0300

    update imports and use new useAppStart hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,27 @@
 import { Routes, Route } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { userSettingsActions } from "./redux/reducers/userSettings";
-import { useDispatch, useSelector } from "react-redux";
-import userSettingsLocalStorage from "./utils/userSettingsLocalStorage";
+import { useAppStart } from "./hooks/useAppStart";
 
-import { AppLayout } from "./pages/layout/AppLayout";
-import { RegisterLocation } from "./pages/RegisterLocation";
-import { Home } from "./pages/Home";
-import { SearchResult } from "./pages/SearchResult";
-import { Page as RecentlySearchedPage } from "./pages/recentlySearched/Page";
-import { Page as SettingsPage } from "./pages/settingsPage/Page";
+import { AppLayout } from "./components/layout/AppLayout";
+import { RegisterLocation } from "./pages/registerLocation/RegisterLocation";
+import { HomePage } from "./pages/home/HomePage";
+import { SearchPage } from "./pages/search/SearchPage";
+import { SearchedLocation } from "./pages/searchedLocation/SearchResult";
+import { SettingsPage } from "./pages/settings/SettingsPage";
 
 function App() {
-  const dispatch = useDispatch();
-  const [isLogin, setIsLogin] = useState(false);
-  const state = useSelector((state) => state.userSettings);
+  const { hasSettingSaved } = useAppStart();
 
-  useEffect(() => {
-    const userSettings = userSettingsLocalStorage.settings();
-    if (userSettings) {
-      dispatch(userSettingsActions.setSettings(userSettings));
-      setIsLogin(true);
-    } else {
-      setIsLogin(false);
-    }
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (state.location.city.length > 0) {
-      userSettingsLocalStorage.setSettings(state);
-      setIsLogin(true);
-    }
-  }, [state]);
-
-  if (!isLogin) {
+  if (!hasSettingSaved) {
     return <RegisterLocation />;
   }
 
   return (
     <Routes>
       <Route path="/" element={<AppLayout />}>
-        <Route index element={<Home />} />
-        <Route
-          path="/search"
-          element={<RecentlySearchedPage></RecentlySearchedPage>}
-        ></Route>
-        <Route
-          path="/search/:query"
-          element={<SearchResult></SearchResult>}
-        ></Route>
-        <Route path="/settings" element={<SettingsPage></SettingsPage>}></Route>
+        <Route index element={<HomePage />} />
+        <Route path="/search" element={<SearchPage />}></Route>
+        <Route path="/search/:query" element={<SearchedLocation />}></Route>
+        <Route path="/settings" element={<SettingsPage />}></Route>
       </Route>
     </Routes>
   );
