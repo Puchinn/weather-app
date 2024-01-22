6f81a0de6059f493984a57dee2ffd7f2ca6b0914
Esteban Sayago <sayagoo823@gmail.com>
2024-01-22 14:45:03 -0300

    extends new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,14 @@
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      "primary-dark": "#161616",
-      "light-primary-dark": "#282828",
-      "heavy-primary-dark": "#141414",
-      "light-text": "#b8b8b8",
-      "primary-dark-hover": "#383838",
+    extend: {
+      colors: {
+        "primary-dark": "#161616",
+        "light-primary-dark": "#282828",
+        "heavy-primary-dark": "#141414",
+        "light-text": "#b8b8b8",
+        "primary-dark-hover": "#383838",
+      },
     },
   },
   plugins: [],
